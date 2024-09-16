931386c97fac486e33932c7fe9fbfc52fbf83bdd
Artem Perehonchuk <artem.perehonchuk@gmail.com>
2024-09-16 14:37:21 +0300

    update tests

--- wdio.xiaomi.conf.js
+++ wdio.xiaomi.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
-    user: process.env.BROWSERSTACK_USERNAME || 'bsuser_vBCR3p',
-    key: process.env.BROWSERSTACK_ACCESS_KEY || 'zCeKwstezez9xywupp74',
+    user: process.env.BROWSERSTACK_USERNAME || 'artem_OihWy2',
+    key: process.env.BROWSERSTACK_ACCESS_KEY || 'm3J6UR26LFaMyzkYkvrv',
   
     updateJob: false,
     specs: [
@@ -14,7 +14,7 @@ exports.config = {
       name: 'browserstack-tests',
       device: 'Xiaomi Redmi Note 9',
       os_version: "10.0",
-      app: process.env.BROWSERSTACK_APP_ID || 'bs://829619f067f2c725ddd9727664f3d8bfac0cccc7',
+      app: process.env.BROWSERSTACK_APP_ID || 'bs://6dd2bf3ec49ee40d38bf3ad8a87321f535a0524f',
       'browserstack.debug': true
     }],
   
